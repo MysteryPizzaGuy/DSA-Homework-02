f1bf548588ecd74f4063c235f997285c61cca2c0
MysteryPizzaGuy <WolfbladeM@gmail.com>
2018-04-29 22:10:58 +0200

    Added counter, fixed updating

--- Conway's Game of Life/Source.cpp
+++ Conway's Game of Life/Source.cpp
@@ -2,6 +2,7 @@
 #include "animate.h"
 #include "gol.h"
 #include <iostream>
+#include <sstream>
 int main() {
 	sf::RenderWindow window(sf::VideoMode(640, 480), "Conway's Game of Life");
 	sf::Clock clock;
@@ -9,9 +10,24 @@ int main() {
 	game.random_seed_start(0.25);
 	animate anim(&window);
 	int generation = 0;
+	sf::Font font;
+	font.loadFromFile("Enter-The-Grid.ttf");
+	sf::Text counter;
+	counter.setCharacterSize(20);
+	counter.setFont(font);
+	std::stringstream ss;
+	sf::Time oneinstancetime = sf::seconds(0.01);// Change the time of one frame here.
 	while (window.isOpen())
 	{
 		//Events
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		if (clock.getElapsedTime()>=oneinstancetime)
+		{
 
 		//Elapsed time get
 
@@ -25,13 +41,17 @@ int main() {
 		//}
 	
 		game.update_all_cells();
+		ss << "Generation >> " << generation;
+		counter.setString(ss.str());
+		ss.str("");
+		generation++;
 		//Render
 		window.clear(game.deadcolor);
 		anim.draw_all_shapes();
+		window.draw(counter);
 		window.display();
-		sf::sleep(sf::Time(sf::seconds(0.01)));
-		std::cout << "Generation: " << generation << std::endl;
-		generation++;
+		clock.restart();
+		}
 	}
 
 
